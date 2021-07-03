3a1854e6c7904ae8d9315aded616ffc161368d6e
Jesse Callahan <jessetylercallahan@gmail.com>
2021-07-03 16:57:47 -0700

    add validation to parameters

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Required]
     public string Family { get; set; }
     public int Age { get; set; }
+    [Required]
     public string Markings { get; set; }
+
   }
 }
